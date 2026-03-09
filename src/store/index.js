cd8fd52db6f2596623d7f027d4c531df1f270038
Ajinkya Kale <kaleajinkya2005@gmail.com>
2026-03-09 14:25:47 +0530

    Edited /store/Index.js for default display

--- src/store/index.js
+++ src/store/index.js
@@ -2,14 +2,14 @@ import { texture } from "three/tsl";
 import { create } from "zustand";
 
 export const useMacBookStore = create((set) => ({
-    color: "#2e2c2e",
+    color: "#adb5bd",
     setColor: (color) => set({ color }),
 
-    scale: 0.08,
+    scale: 0.06,
     setScale: (scale) => set({ scale }),
 
     texture: "/videos/feature-1.mp4",
     setTexture: (texture) => set({ texture }),
 
-    reset: () => set({ color: '#2e2c2e', scale: 0.08, texture: "/videos/feature-1.mp4" }),
+    reset: () => set({ color: '#adb5bd', scale: 0.06, texture: "/videos/feature-1.mp4" }),
 }))
